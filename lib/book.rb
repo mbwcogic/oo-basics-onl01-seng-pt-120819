ff7b79f2877ec1602f1ca8cdd7f1050ddadee374
Michael Waldo <mbwcogic@yahoo.com>
2020-01-12 23:45:00 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,29 +1,14 @@
 class Book
- def initialize(title)
-      @title = title
-  end 
-   def title
-      @title
-  end
-    def author=(author)
-    @author = author
-  end
-   def author
-    @author
-  end
-   def page_count=(num)
-    @page_count = num
-  end
-   def page_count
-    @page_count
-  end
-  def genre=(genre)
-    @genre = genre
+  attr_accessor :author, :page_count, :genre
+ 
+  def initialize(title)
+    @title = title
   end
  
-  def genre
-    @genre
+  def title
+    @title
   end
+ 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
